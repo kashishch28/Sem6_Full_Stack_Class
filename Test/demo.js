8cbe2197a42048316160e754a777d88dd5848f3a
kashishch28 <155078004+kashishch28@users.noreply.github.com>
2026-02-09 09:19:02 +0530

    update demo and math files

--- Test/demo.js
+++ Test/demo.js
@@ -3,4 +3,13 @@ function add(a,b){
 }
 
 add();
-export default add;
+
+module.exports = add;
+//export default add;
+
+//if multiple functions are there then we can export them as an object like this
+// module.exports = {
+//     add,
+//     subtract,
+//     multiply
+// }
--- Test/math.js
+++ Test/math.js
@@ -1,3 +1,9 @@
-import add from './demo.js';
+// import add from './demo.js';
+
+// console.log(add(2,3));
+
+const add = require('./demo.js');
 
 console.log(add(2,3));
+
+//difference in type module and type common js is that in type module we can use import and export and in type commonjs we can use require and module.exports
