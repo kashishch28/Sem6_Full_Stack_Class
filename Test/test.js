1eb3040f955a041706c90ada783957f88c671c52
kashishch28 <155078004+kashishch28@users.noreply.github.com>
2026-02-16 09:36:49 +0530

    commit

--- Test/test.js
+++ Test/test.js
@@ -26,36 +26,36 @@
 
 
 
-setTimeout(() => {
-    console.log("1");
-    setTimeout(() => {
-        console.log("2");
-        setTimeout(() => {
-            console.log("3");
-            setTimeout(() => {
-                console.log("4");
-              },2000);  
-        },2000);
-    },2000);
-},2000);
+// setTimeout(() => {
+//     console.log("1");
+//     setTimeout(() => {
+//         console.log("2");
+//         setTimeout(() => {
+//             console.log("3");
+//             setTimeout(() => {
+//                 console.log("4");
+//               },2000);  
+//         },2000);
+//     },2000);
+// },2000);
 
 //it causes callback hell and makes code difficult to read and maintain. To avoid this, we can use Promises or async/await for better readability and error handling.
 
 ///->Promise
 
-const promise = new Promise((resolve,reject)=>{
-  let deliver = true;
-  if(deliver){
-    resolve("Package delivered successfully");
-  }else{
-    reject("Package delivery failed");
-  }     
+// const promise = new Promise((resolve,reject)=>{
+//   let deliver = true;
+//   if(deliver){
+//     resolve("Package delivered successfully");
+//   }else{
+//     reject("Package delivery failed");
+//   }     
 
-});
+// });
 
-promise
-  .then(()=> console.log("Ye mera hua resolve"))
-  .catch(()=> console.log("Ye mera hua reject"));
+// promise
+//   .then(()=> console.log("Ye mera hua resolve"))
+//   .catch(()=> console.log("Ye mera hua reject"));
 
 
 
@@ -74,7 +74,10 @@ promise
 
 
 const fs = require('fs');
-fs.writeFileSync("file.txt","utf-8",function(err,data) {
-  if(err) console.log(err);
-  console.log(data);
+fs.readFile("file.txt","utf-8",function(err,data) {
+  if(err){
+    console.log("Error reading file:", err);
+  } else {
+    console.log(data);
+  }
 });
